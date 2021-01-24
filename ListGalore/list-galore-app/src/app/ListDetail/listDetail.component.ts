8a40f3ec2c68c4dbe93edbccb0dddd50bbe4e4d2
manofgate7 <manofgate@gmail.com>
2021-01-23 23:59:20 -0700

    init basic list functions

--- ListGalore/list-galore-app/src/app/ListDetail/listDetail.component.ts
+++ ListGalore/list-galore-app/src/app/ListDetail/listDetail.component.ts
@@ -14,4 +14,46 @@ export class ListDetailComponent {
 			ListItems: ["ham", "mayo", "dog food"]
 		} as iList;
 		
+		showAddFeature:boolean = false;
+
+		newItem:string = "";
+		isRemovingItems: boolean = false;
+		isEditingItems:boolean = false;
+		itemSelected:string  = "";
+		editedItem: string ="";
+		
+		AddToList(): void {
+			this.showAddFeature = !this.showAddFeature;
+		}
+
+		RemoveFromList(): void {
+			this.isRemovingItems = !this.isRemovingItems;
+		}
+		EditFromList(): void {
+			this.isEditingItems = !this.isEditingItems;
+		}
+
+		SaveToList(): void {
+			this.currentList.ListItems.push(this.newItem);
+			this.newItem = "";
+		}
+		Remove(item: string): void{
+			var index = this.currentList.ListItems.indexOf(item, 0);
+			if (index > -1) {
+				this.currentList.ListItems.splice(index, 1);
+			}
+		}
+
+		EditItem(item:string) {
+			this.editedItem = item;
+			this.itemSelected = item;
+		}
+
+		SaveEditItem(): void {
+			var index = this.currentList.ListItems.indexOf(this.itemSelected, 0);
+			if (index > -1) {
+				this.currentList.ListItems[index] = this.editedItem;
+			}
+			
+		}
 }
